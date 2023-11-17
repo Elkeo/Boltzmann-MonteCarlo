ccfb33d05c1076e74545316c0ed641f2caae680e
Guillaume Doyen <107686092+DoyenGuillaume@users.noreply.github.com>
2023-11-17 11:09:57 +0100

    Début du code

---
+++ Code/MonteCarlo.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+class fonction_g_sin {
+ private:
+  //amplitude du sinus
+  const double _amplitude;
+  //pulsation du sinus
+  const double _pulsation;
+
+ public:
+  //Constructeurs :
+  fonction_g_sin(const double amplitude, const double pulsation);
+  fonction_g_sin(const fonction_g_sin& fonction_g_sin);
+  //Evaluation:
+  double compute_g_x(const double var_x);
+
+  //Fonctions d'accès :
+  const double& getAmplitude() const {return _amplitude;};
+  const double& getPulsation() const {return _pulsation;};
+};
+
---
+++ Code/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+#include "MonteCarlo.cpp"
+
+int main()
+{
+  double amplitude = 25.0;
+  double pulsation = 3.0;
+  double x = 0.25;
+
+  fonction_g_sin fonction_sinus1(amplitude, pulsation);
+
+  fonction_g_sin fonction_sinus2(fonction_sinus1);
+
+  double res = fonction_sinus2.compute_g_x(x);
+
+  std::cout << "Le résultat de "<< x << "par la fonction est" << res << "!!" << std::endl;
+
+  return 0;
+}
