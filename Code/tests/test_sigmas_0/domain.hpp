23afb392e26b6d47ffdc5f4eb536bff10a6e4899
EL MADDAH Khaoula <kelmaddah@ssh.enseirb-matmeca.fr>
2024-01-06 09:11:14 +0100

    test sigmaT=sgmaS=0

---
+++ Code/tests/test_sigmas_0/domain.hpp
@@ -0,0 +1,55 @@
+#ifndef DOMAIN_HPP
+#define DOMAIN_HPP
+
+#include "main.hpp"
+
+
+/* CLASSE DÉFINISSANT LE DOMAINE DANS LEQUEL LE PAQUET DE PARTICULE ÉVOLUE */
+class GenericDomain
+{
+protected:
+   int _d;
+   std::valarray<Vecteur> _Omega;
+   std::valarray<Vecteur> _xCoords, _yCoords;
+
+public:
+   /* Constructeurs et destructeurs par défaut */
+   GenericDomain(const int& nbDim, const std::valarray<Vecteur>& Om);
+   virtual ~GenericDomain();
+
+   /* Fonctions membres */
+   double initialCondition(const Vecteur&, const Vecteur&) const;
+   virtual void applyBoundaryConditions(Vecteur&, double&, Vecteur&) const = 0;
+   void applyInitialCondition() const;
+   double sigmaS(const Vecteur&, const double&, const Vecteur&) const;
+   double sigmaT(const Vecteur&, const double&, const Vecteur&) const;
+   double sampleTau(const Vecteur&, const double&, const Vecteur&) const;
+   Vecteur sampleVprime(const Vecteur&, const double&, const double&, const Vecteur&) const;
+
+   /* Accesseurs */
+   const int& get_d() const { return _d; }
+   const std::valarray<Vecteur>& get_Omega() const { return _Omega; }
+};
+
+/* Domaine avec conditions de bords périodiques */
+class PeriodicDomain : public GenericDomain
+{
+public:
+   /* Constructeurs et destructeurs par défaut */
+   PeriodicDomain(const int& nbDim, const std::valarray<Vecteur>& Om) : GenericDomain(nbDim, Om) {};
+   virtual ~PeriodicDomain();
+
+   virtual void applyBoundaryConditions(Vecteur&, double&, Vecteur&) const;
+};
+
+/* Domaine avec choc élastique aux parois */
+class ElasticDomain : public GenericDomain
+{
+public:
+   /* Constructeurs et destructeurs par défaut */
+   ElasticDomain(const int& nbDim, const std::valarray<Vecteur>& Om) : GenericDomain(nbDim, Om) {};
+   virtual ~ElasticDomain();
+
+   virtual void applyBoundaryConditions(Vecteur&, double&, Vecteur&) const;
+};
+#endif  // DOMAIN_HPP
---
+++ Code/tests/test_sigmas_0/parametres.cpp
@@ -0,0 +1,5 @@
+#include "parametres.hpp"
+
+const double Parametres::SigmaS = 0.0;
+const double Parametres::SigmaT = 0.0;
+const double Parametres::modV = 1.0;
