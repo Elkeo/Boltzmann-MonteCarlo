50a8a2542241ceff3c9a9350255ff03e412cabe2
Guillaume Doyen <107686092+DoyenGuillaume@users.noreply.github.com>
2024-01-09 10:34:56 +0100

    Revert "Param toml guillaume"

--- Code/MC/domain.cpp
+++ Code/MC/domain.cpp
@@ -1,9 +1,7 @@
 #include "domain.hpp"
-#include "parameters.hpp"
+#include "parametres.hpp"
 
-GenericDomain::GenericDomain(const struct struct_parameters *parameters, const std::valarray<Vecteur>& Om) : 
-_Omega(Om),
-_parameters(parameters)
+GenericDomain::GenericDomain(const int& nbDim, const std::valarray<Vecteur>& Om) : _d(nbDim), _Omega(Om)
 {
 };
 
@@ -23,16 +21,16 @@ PeriodicDomain::~PeriodicDomain()
 void ElasticDomain::applyBoundaryConditions(Vecteur& xp, double& sp, Vecteur& vp) const
 {
    // Choc élastique sur les parois
-   for (int i = 0; i < this->_parameters->nbDims; i++)
+   for (int i = 0; i < _d; i++)
    {
-      if (xp[i] < this->_Omega[i][0])
+      if (xp[i] < _Omega[i][0])
       {
-         xp[i] = this->_Omega[i][0];
+         xp[i] = _Omega[i][0];
          vp[i] *= -1;
       }
-      else if (xp[i] > this->_Omega[i][1])
+      else if (xp[i] > _Omega[i][1])
       {
-         xp[i] = this->_Omega[i][1];
+         xp[i] = _Omega[i][1];
          vp[i] *= -1;
       }
    }
@@ -40,15 +38,15 @@ void ElasticDomain::applyBoundaryConditions(Vecteur& xp, double& sp, Vecteur& vp
 
 void PeriodicDomain::applyBoundaryConditions(Vecteur& xp, double& sp, Vecteur& vp) const
 {
-   for (int i = 0; i < this->_parameters->nbDims; i++)
+   for (int i = 0; i < _d; i++)
    {
-      if (xp[i] < this->_Omega[i][0])
+      if (xp[i] < _Omega[i][0])
       {
-         xp[i] += (this->_Omega[i][1] - this->_Omega[i][0]);
+         xp[i] += (_Omega[i][1] - _Omega[i][0]);
       }
-      else if (xp[i] > this->_Omega[i][1])
+      else if (xp[i] > _Omega[i][1])
       {
-         xp[i] -= (this->_Omega[i][1] - this->_Omega[i][0]);
+         xp[i] -= (_Omega[i][1] - _Omega[i][0]);
       }
    }
 };
@@ -61,13 +59,13 @@ double GenericDomain::initialCondition(const Vecteur& x, const Vecteur& v) const
 
 double GenericDomain::sigmaS(const Vecteur& xp, const double& t, const Vecteur& vp) const
 {
-   double sigmas = this->_parameters->sigmaS;
+   double sigmas = Parametres::SigmaS;
    return sigmas;
 };
 
 double GenericDomain::sigmaT(const Vecteur& xp, const double& t, const Vecteur& vp) const
 {
-   double sigmat = this->_parameters->sigmaT;
+   double sigmat = Parametres::SigmaT;
    return sigmat;
 };
 
@@ -81,21 +79,21 @@ Vecteur GenericDomain::sampleVprime(const Vecteur& xp, const double& sp, const d
    // Création du générateur de nombre aléatoires
    std::default_random_engine generator;
 
-   if (this->_parameters->nbDims == 1)
+   if (_d == 1)
    {
       // tirage uniforme sur [-1, 1]
       std::uniform_real_distribution<double> distribution(-1, 1);
 
       vprime = { normV * distribution(generator) };
    }
-   else if (this->_parameters->nbDims == 2)
+   else if (_d == 2)
    {
       // tirage uniforme sur le cercle unité
       std::uniform_real_distribution<double> distribution(0, 2 * M_PI);
       double theta = distribution(generator);
       vprime = { normV * cos(theta), normV * sin(theta) };
    }
-   else if (this->_parameters->nbDims == 3)
+   else if (_d == 3)
    {
       // tirage uniforme sur la sphère unité (composition de loi normales)
       // voir Daniel Saada - lois uniformes sur la sphère
@@ -117,6 +115,5 @@ double GenericDomain::sampleTau(const Vecteur& xp, const double& sp, const Vecte
    std::default_random_engine generator;
    std::uniform_real_distribution<double> distribution(0, 1);
 
-   return -log(distribution(generator)) / this->_parameters->sigmaT*sqrt(((vp * vp).sum()));
-
+   return -log(distribution(generator)) / (sigmaT(xp, sp, vp) * sqrt((vp * vp).sum()));
 };
