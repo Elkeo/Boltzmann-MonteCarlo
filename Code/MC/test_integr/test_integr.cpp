df22c2f921b6a710fc60c30741dd6e292722198c
Clement16A <148045452+Clement16A@users.noreply.github.com>
2024-01-15 17:44:35 +0100

    maj du test intégrale

--- Code/MC/test_integr/test_integr.cpp
+++ Code/MC/test_integr/test_integr.cpp
@@ -1,5 +1,10 @@
 #include "../main.hpp"
 #include "../population.hpp"
+#include <cmath>
+
+
+using namespace std;
+
 
 /*
   code.
@@ -12,28 +17,74 @@ int main(int argc, char const* argv[])
 
    struct struct_parameters parameters;
    init_parameters(parameters);
+
+   /* Ouverture d'un fichier pour stocker la solution */
+   std::ofstream file("solution_t_" + std::to_string(parameters.time) + ".txt");
+
    /* Création du domaine où les particules se meuvent */
-
-
    std::valarray<Vecteur> Omega(parameters.nbDims);
-   for (int i = 0; i < parameters.nbDims; i++)
-   {
-      Vecteur dimDomain = { -1, 1 };
-      Omega[i] = dimDomain;
+   Omega[0] = parameters.array_x;
+   if (parameters.nbDims >= 2) {
+      Omega[1] = parameters.array_y;
+      if (parameters.nbDims == 3) {
+         Omega[2] = parameters.array_z;
+      }
    }
+
    GenericDomain* Domaine = NULL;
-   Domaine = new PeriodicDomain(parameters, Omega);
+   if (parameters.domainType == "elastic")
+   {
+      Domaine = new ElasticDomain(parameters, Omega);
+   }
+   else if (parameters.domainType == "periodic")
+   {
+      Domaine = new PeriodicDomain(parameters, Omega);
+   }
+   else
+   {
+      delete Domaine;
+      std::cout << "Domain type not recognized : choose \"elastic\" or \"periodic\"." << std::endl;
+      exit(1);
+   }
 
    /* Création de la solution u(x, t, v) */
-   double u(0.0);
+   std::valarray<std::valarray<std::valarray<double>>> u(std::valarray<std::valarray<double>>(std::valarray<double>(0.0, parameters.nbPtsZ), parameters.nbPtsY), parameters.nbPtsX);
+   double somme=0;
+   for (int i = 0; i < parameters.nbPtsX; i++)
+   {
+      for (int j = 0; j < parameters.nbPtsY; j++)
+      {
+         for (int k = 0; k < parameters.nbPtsZ; k++)
+         {
+            double x = parameters.array_x[0] + (i + 0.5) * (parameters.array_x[1] - parameters.array_x[0]) / (parameters.nbPtsX);
+            double y = parameters.array_y[0] + (j + 0.5) * (parameters.array_y[1] - parameters.array_y[0]) / (parameters.nbPtsY);
+            double z = parameters.array_z[0] + (k + 0.5) * (parameters.array_z[1] - parameters.array_z[0]) / (parameters.nbPtsZ);
 
-   /* Création de la population de particules (fictives) */
-   Population packOfParticles(Domaine, parameters, u);
+            /* Création de la population de particules (fictives) */
+            
+            Population packOfParticles(Domaine, parameters, u[i][j][k], { x, y, z });
 
-   /* Les particules (fictives) évoluent */
-   packOfParticles.move();
+            /* Les particules (fictives) évoluent */
+            packOfParticles.move();
 
-   /* On en déduit la solution u(x, t, v) */
+            /* On en déduit la solution u(x, t, v) */
+            u[i][j][k] = packOfParticles.get_u();
+            somme+=u[i][j][k];
+         }
+      }
+   }
+
+   Vecteur x(3); 
+   Vecteur v(3); 
+
+   
+   double U0 = Domaine->initialCondition(x, v);
+
+
+   double U=somme*(parameters.array_x[1]-parameters.array_x[0])*(parameters.array_y[1]-parameters.array_y[0])*(parameters.array_z[1]-parameters.array_z[0]);
+
+   cout << "erreur"<<U-(U0*exp(-parameters.modV*(parameters.sigmaS-parameters.sigmaT)*parameters.time))<< endl;
+   delete Domaine;
 
 
    return 0;
