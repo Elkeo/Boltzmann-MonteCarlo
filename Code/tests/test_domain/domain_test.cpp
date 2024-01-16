7a023c8f972714af2aa4ed8f80b10e8e32f3120f
Elkeo <antoine.boucher@enseirb-matmeca.fr>
2024-01-16 17:42:17 +0100

    reorganization

--- Code/tests/test_domain/domain_test.cpp
+++ Code/tests/test_domain/domain_test.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+#include "../googletest/googletest/include/gtest/gtest.h"
 #include "domain.hpp"
 #include "parametres.hpp"
 
@@ -10,24 +10,24 @@ protected:
     }
 
     void TearDown() override {
-        
+
     }
 
-    
+
     Domain domain;
 };
 
 // Test de la fonction u0
 TEST_F(DomainTest, TestU0) {
-    Vecteur xp(3, 0.0);  
-    Vecteur vp(3, 1.0); 
+    Vecteur xp(3, 0.0);
+    Vecteur vp(3, 1.0);
 
     double result = domain.u0(xp, vp);
 
     EXPECT_DOUBLE_EQ(result, 0.0);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
