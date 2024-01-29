24c2d6935aa30506c6fb44051f7836d9ddde497b
Clement16A <148045452+Clement16A@users.noreply.github.com>
2024-01-29 23:29:10 +0100

    BNR fonctionnelle personnne la modifie svp

--- Code/MC/BNR/BNR.py
+++ Code/MC/BNR/BNR.py
@@ -9,39 +9,90 @@ YELLOW = "\033[33m"
 BOLD = "\033[1m"
 RESET = "\033[0m"
 
-# Liste des répertoires contenant les codes C++ et Makefiles
-repertoires = [
-    "./Test_echantillonnage",
-    # "./tests",
-    "./test_integr",
-    "./repro"
+#################################################################
+#C++
+#################################################################
+# repertoires_C = [
+#     "./Test_echantillonnage",
+#     "./repro"
+
+# ]
+
+# # Boucle sur chaque répertoire
+# for chemin in repertoires_C:
+#     print(f"{YELLOW}Compilation et éxecution de : {chemin}")
+
+#     # Changement du répertoire courant au répertoire du test
+#     os.chdir(chemin)
+
+#     # Compilation du projet avec Makefile
+#     compile_process = subprocess.run(["make"], capture_output=True, text=True)
+
+#     if compile_process.returncode == 0:
+#         print(f"{GREEN}Compilation réussie.")
+#         print(f"{BLUE}{BOLD}####################################{RESET}\n")
+
+#         os.system("./run")  
+#     else:
+#         print(f"{RED}Erreur lors de la compilation du projet dans {chemin}:\n{compile_process.stderr}{RESET}")
+    
+#     if chemin=="./Test_echantillonnage":
+#         os.system("python3 treatment.py")  
+
+
+#     print(f"{BLUE}{BOLD}\n####################################{RESET}")
+#     # Nettoyage du projet avec make clean
+#     clean_process = subprocess.run(["make", "clean"], capture_output=True, text=True)
+#     if clean_process.returncode == 0:
+#         print(f"{GREEN}Nettoyage réussi.{RESET}")
+#     else:
+#         print(f"{RED}Erreur lors du nettoyage du projet dans {chemin}:\n{clean_process.stderr}{RESET}")
+
+#     # Retour au répertoire précédent pour la prochaine itération
+#     os.chdir("..")
+
+
+
+#################################################################
+#Python
+#################################################################
+
+repertoires_python = [
+    "./advection"
 
 ]
 
+paramfile=["./Tests/01\ -\ advection.toml","./Tests/02\ -\ exponentiel.toml"]
+i=0
+
 # Boucle sur chaque répertoire
-for chemin in repertoires:
+for chemin in repertoires_python:
     print(f"{YELLOW}Compilation et éxecution de : {chemin}")
 
     # Changement du répertoire courant au répertoire du test
-    os.chdir(chemin)
+    os.chdir("../")
 
     # Compilation du projet avec Makefile
     compile_process = subprocess.run(["make"], capture_output=True, text=True)
 
     if compile_process.returncode == 0:
-        print(f"{GREEN}Compilation réussie.")
+        print(f"{GREEN}Compilation du main réussie.")
         print(f"{BLUE}{BOLD}####################################{RESET}\n")
+        
+        os.system("./main "+paramfile[i])  
 
-        os.system("./run")  
+        os.chdir("./BNR"+chemin[1:])
+        os.system("python3 run.py")  
     else:
         print(f"{RED}Erreur lors de la compilation du projet dans {chemin}:\n{compile_process.stderr}{RESET}")
     
-    if chemin=="./Test_echantillonnage":
-        os.system("python3 treatment.py")  
+ 
 
 
     print(f"{BLUE}{BOLD}\n####################################{RESET}")
     # Nettoyage du projet avec make clean
+    os.chdir("../../")
+    i+=1
     clean_process = subprocess.run(["make", "clean"], capture_output=True, text=True)
     if clean_process.returncode == 0:
         print(f"{GREEN}Nettoyage réussi.{RESET}")
@@ -49,7 +100,19 @@ for chemin in repertoires:
         print(f"{RED}Erreur lors du nettoyage du projet dans {chemin}:\n{clean_process.stderr}{RESET}")
 
     # Retour au répertoire précédent pour la prochaine itération
-    os.chdir("..")
+    os.chdir("./BNR")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 print(f"{BOLD}\nTous les tests on été effectués.")
